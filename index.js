ffc15bdde195364456e4635684b1791f914ddcdd
Toluwaa-o <121364075+Toluwaa-o@users.noreply.github.com>
2023-01-19 00:58:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let marker = document.getElementById("allbtn");
+let unread = document.getElementsByClassName("unread");
+let stats = document.getElementById("stat");
+let dots = document.querySelectorAll(".dot");
+let notifs = document.querySelectorAll(".notif");
+stats.textContent = unread.length
+
+for(let i = 0; i < notifs.length; i++){
+    notifs[i].addEventListener("click", function(){
+        notifs[i].classList.remove("unread");
+        dots[i].style.display = "none";
+        reCount()
+    })
+}
+
+marker.addEventListener("click", function(e) {
+    e.preventDefault();
+    for(let i = 0; i < notifs.length; i++) {
+        notifs[i].classList.remove("unread");
+        dots[i].style.display = "none";
+        reCount()
+    }
+})
+
+function reCount(){
+    stats.textContent = unread.length
+}
